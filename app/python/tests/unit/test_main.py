a12f34a318c1ebfc0bea499018eeb0a070ab187e
davismatrix <davismatrix@gmail.com>
2025-04-16 17:18:13 -0400

    adding unit tests steps-v2

--- app/python/tests/unit/test_main.py
+++ app/python/tests/unit/test_main.py
@@ -3,17 +3,21 @@ from app.python.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
+
 
 def test_health_check():
     response = client.get("/healthz")
     assert response.status_code == 200
     assert response.json() == {"health": "ok"}
 
+
 def test_readiness_check():
     response = client.get("/ready")
     assert response.status_code == 200
     assert response.json() == {"ready": "ok"}
+
